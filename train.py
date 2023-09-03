a2c727b912966b01d202d99f4e2fb16b750dcbaf
Lily <87243032+ElleLeonne@users.noreply.github.com>
2023-09-02 19:25:27 -0500

    Dev (#7)
    * Update README.md
    
    * Update lightning_wrapper.py
    
    Axeing torchvision dependencies
    
    * Create requirements.txt
    
    * Delete base_functions.py
    
    Already integrated
    
    * Update lightning_wrapper.py
    
    Since tensorboard is not included in base Lightning now, migrating to internal logger for the time being.
    
    Would rather implement W&B anyways.
    
    * Update lightning_wrapper.py
    
    * Update train.py
    
    Tests fine with  a classification model, still need to work on live-testing w/ llama
    
    * Update README.md

--- train.py
+++ train.py
@@ -1,67 +1,39 @@
-"""
-Basic trainer template for multi-gpu, just so we have scaffolding to work from.
-Lifted from -> https://github.com/ibeltagy/pytorch-lightning/blob/master/pl_examples/basic_examples/gpu_template.py
-"""
+import torch
 import os
 from argparse import ArgumentParser
 from transformers import LlamaForCausalLM
 from peft import LoraConfig
 
-from pytorch_lightning import Trainer, seed_everything
+from datasets import load_dataset, Dataset
+import lightning as L
 from training.lightning_wrapper import ReloraModule, lora_modules
 
-seed_everything(234)
-
+# Library variables
+L.seed_everything(234)
+torch.set_float32_matmul_precision("medium")
 
 def main(args):
-    """ Main training routine specific for this project. """
-    # ------------------------
-    # 1 INIT LIGHTNING MODEL
-    # ------------------------
+    """ We need to find a way to integrate this with Lightning's CLI object, ideally. """
+    # 1: Init datasets
+    train_set = load_dataset("vicgalle/alpaca-gpt4", split="train")
+    train_set = DataLoader(train_set, batch_size=1)
+    val_set = load_dataset("vicgalle/alpaca-gpt4", split="eval")
+    val_set = DataLoader(val_set, batch_size=1)
+    
+    # 2: Init model
+    lora_config = LoraConfig(r=8, lora_alpha=16, lora_dropout=0.1, bias="none", inference_mode=False, modules_to_save="RMSNorm",
+                             target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "down_proj", "up_proj", "wte", "lm_head"])
+
     model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b",
                                              load_in_8bit=True,)
     model = ReloraModule(model=model,
-                         lora_config=LoraConfig(**vars(args)),
-                        **vars(args))
+                         lora_config=lora_config,
+                         train_set = train_set,
+                         eval_set = val_set,)
 
-    #Elle's notes - ex:
-    #model = model.from_pretrained(path)
-    # -- OR --
-    #model = model(config).init_weights()
-    #-- THEN --
-    """model = LightningModel(model=model,
-                            optimizer=optimizer,
-                            etc)"""
-
-    # Begin training
-    trainer = Trainer.from_argparse_args(args)
+    # 3: Begin training
+    trainer = L.Trainer(max_epochs=10)
     trainer.fit(model)
 
-#Elle's note - I'm unsure if CLI will work with "Bring your own model". We could either build it on top of Llama or GPTnano or something,
-# or keep it hackable and general. Or both.
-def run_cli():
-    # ------------------------
-    # TRAINING ARGUMENTS
-    # ------------------------
-    # these are project-wide arguments
-    root_dir = os.path.dirname(os.path.realpath(__file__))
-    parent_parser = ArgumentParser(add_help=False)
-
-    # Establish your custom CLI variables in the module itself
-    parser = ReloraModule.add_model_specific_args(parent_parser, root_dir)
-    parser = Trainer.add_argparse_args(parser)
-    parser.set_defaults(gpus=2)
-    parser.set_defaults()
-
-    # Initializing args
-    args = parser.parse_args()
-    args.lora_target_modules = lora_modules[args.lora_modules] #Set lora_modules from our lookup dictionary
-
-    # ---------------------
-    # RUN TRAINING
-    # ---------------------
-    main(args)
-
-
 if __name__ == '__main__':
-    run_cli()
+    main()
