2965847f5fffa064877f8e7ad6ff7b3cd0f188ca
Lily <87243032+ElleLeonne@users.noreply.github.com>
2023-09-06 12:46:02 -0500

    Update plugins.py

--- training/plugins.py
+++ training/plugins.py
@@ -1,16 +1,13 @@
 from torch.nn import Module
 from torch.optim import Optimizer
 from lightning.pytorch.plugins.precision import PrecisionPlugin
-from peft import prepare_model_for_kbit_training
 
-class Precision(PrecisionPlugin):
+class Int8Precision(PrecisionPlugin):
   """This exists only to stop Lightning from attempting to use its own precision settings"""
   def __init__(self) -> None:
     super().__init__()
-    self.precision = "8"
-    def connect(self, model: Module, optimizers: List[Optimizer], lr_schedulers: List[Any]
-      ) -> Tuple[Module, List[Optimizer], List[Any]]:
-
-      # Of note, this presumes to use gradient checkpointing.
-      model = prepare_model_for_kbit_training(model)
-      return model, optimizers, lr_schedulers
+    self.precision = "int8"
+    
+  def connect(self, model: Module, optimizers: List[Optimizer], lr_schedulers: List[Any]) -> Tuple[Module, List[Optimizer], List[Any]]:
+    model.trainer.__dict__["precision"] = "int8"
+    return model, optimizers, lr_schedulers
