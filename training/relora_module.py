f78e85777462bd7f80364fdb7d966835e4b333b2
Lily Erickson <contact@lilyerickson.us>
2024-03-02 15:33:48 -0600

    Update relora_module.py
    Merging modifications from Cross-Axis Transformer (CAT) self-published research paper, added some new Lightning hooks.
    
    Modifications to facilitate LLM style training.

--- training/relora_module.py
+++ training/relora_module.py
@@ -1,41 +1,39 @@
-import os
 import sys
 import math
 import torch
 import torch.nn as nn
 import lightning as L
 from torch import optim
-from torch.utils.data import DataLoader
-from typing import Dict, Any
-#from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
-from .utils import create_module_lists, get_weight_names
+import torch.utils.data as td
+from typing import Any
+from .utils import Accuracy
 
 import torch._dynamo # Auto-resolve torch.compile errors.
 torch._dynamo.config.suppress_errors = True
+
 peft_supported_layers = [""]
 
-class ReloraModule(L.LightningModule):
+class LightningModel(L.LightningModule):
     """ Relora base class for inheritance. """
-    def __init__(self,
-                 model_class: nn.Module,
-                 model_path: str,
-                 lora_config: Any = None,
-                 lora_merge_freq: int = 0,
-                 train_dataset: Any = None,
-                 eval_dataset: Any = None,
-                 batch_size: int = 1,
-                 learning_rate: float = 3e-4,
-                 num_workers: int = 4,
-                 **kwargs):
-        
-        super().__init__()
+    def __init__(self, model_class: nn.Module, model_path: str = None, collator = None, lora_merge_freq: int = 0, learning_rate: float = 3e-4,
+                 train_dataset: Any = None, eval_dataset: Any = None, test_dataset: Any = None, batch_size: int = 1, num_workers: int = 4, **kwargs):
+        """ Args:
+
+            - model_class: Either a preloaded model object, or a base class that you intend to train with. Using a full object limits precision tricks.
+            - model_path: The path to load the model from if it's a huggingface model. 'None' assumes model_class is a pre-loaded model object.
+            - lora_config: The configuration file to use when re-initializing LoRA. 'None' disables ReLoRA and runs normal training. 
+            - lora_merge_freq: How many epochs are required before merging and resetting the LoRA layers into the base model. 
+            - learning_rate: How quickly the weights update. LoRA is very sensitive to learning rate, and the layers overfit quite quickly.
+            - train/eval/test_dataset: Dataset objects that you intend to train on. Override self.collator in subclasses for custom batching behavior.
+            - batch_size: How many samples per batch for the dataloaders to use. Setting this higher speeds up training, but uses more memory.
+            - num_workers: How many workers the Dataloader should launch to preprocess your datasets, larger batch sizes require more workers.
+            """
+        super().__init__(**kwargs)
         # Paths for save-state juggling
-        checkpoint_folder = "relora_checkpoints"
-        if not os.path.exists(checkpoint_folder):
-            os.makedirs(checkpoint_folder)
-        self.model_class = model_class
+        self.model_class = model_class # The trainer will understand if you pass an instantiated model
         self.model_path = model_path
-        self.save_path = f"./{checkpoint_folder}/{model_path}"
+        self.collator = collator # If your model uses a custom batching function, you'll want to set this in your subclass.
+        self.accuracy = Accuracy()
 
         # Training and optimization
         self.learning_rate = learning_rate
@@ -45,18 +43,25 @@ class ReloraModule(L.LightningModule):
         # Datasets
         self.train_set = train_dataset
         self.eval_set = eval_dataset
-
-        # Misc Arguements
-        self.num_workers = num_workers
+        self.test_set = test_dataset
         self.batch_size = batch_size
-        
+        self.num_workers = num_workers
+ 
         # LoRA settings
-        self.torch_compile = False # You can set this flag to False if torch.dynamo fails for you.
-        self.config = lora_config
-        self.merge_freq = lora_merge_freq
-        self.merge_precision = torch.float32 # A variable for future tests. Unused right now.
-        self.load_precision = torch.float16 # Peft will cast this to float16 on its own, anyways.
-        self.train_all_params = True # Whether to automatically set training mode on all non-LoRA weights or not.
+        self.torch_compile = True # You can set this flag to False if torch.dynamo fails for you, to avoid using pytorch 2.0's compile method.
+        self.verbose = True
+    # ---------------------
+    # TRAINING LOOP
+    # ---------------------
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, input_dc, batch_idx):
+        print("Please implement the 'training_step' method in a subclass of this parent class.")
+        return None
+    def validation_step(self, input_dc, batch_idx):
+        print("Please implement the 'validation_step' method in a subclass of this parent class.")
+        return None
 
     # ---------------------
     # TRAINING SETUP
@@ -65,42 +70,39 @@ class ReloraModule(L.LightningModule):
         """Called before trainer.fit() and trainer.eval()"""
         if stage == 'fit':
             self.load_model(self.model_path)
-            self.prepare_lora_training()
-
-            if torch.__version__ >= "2" and sys.platform != "win32" and self.torch_compile:
+            if torch.__version__ <= "2":
+                print("Pytorch's compiler is not compatible with versions lower than 2.0 - Performing standard training.")
+            elif sys.platform == "win32":
+                print("Pytorch's compiler is only compatible with 64 bit versions of Windows. - Performing standard training.")
+            elif sys.version_info.major == 3 and sys.version_info.minor >= 12:
+                print("Pytorch's compiler is not yet compatible with Python " + str(sys.version_info.major)+"."+str(sys.version_info.minor) + " - Performing standard training.")
+            elif self.torch_compile is False:
+                print("torch.compile has been manually disabled. - Performing standard training.")
+            else:
                 self.model = torch.compile(self.model)
+            self.model = self.model.train() # Huggingface explicitly requires this now.
 
-            checkpoint_folder = self.trainer.default_root_dir # Create our logging folder.
-            if not os.path.exists(checkpoint_folder):
-                os.makedirs(checkpoint_folder)
-            self.save_path = f"./{checkpoint_folder}/{self.model_path}"
+        if stage == 'eval':
+            self.model = self.model.eval()
 
-    def prepare_lora_training(self):
-        """ Generates compatible lora_modules list automatically, as well as non-lora modules list. """
-        if self.merge_freq > 0: # We use this as our ReLoRA flag.
-            if self.config.target_modules is None: # Whether to automatically generate all valid target modules
-                self.config.target_modules, self.base_modules = create_module_lists(self.model, "lora")
-            else: # Or to just stick with figuring out the non-LoRA layers
-                self.base_modules = [item for item in get_weight_names() if item not in self.config.target_modules]
-            self.base_modules += ["lora_"]
-            self.init_lora()
-    
     def configure_optimizers(self):
         """ Return whatever optimizers and learning rate schedulers you want here. At least one optimizer is required. """
-        print(self.max_steps)
+        print("Preparing optimizers")
         optimizer = optim.AdamW(self.parameters(), lr=self.learning_rate)
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, self.trainer.max_steps)
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, self.max_global_steps)
         return {"optimizer": optimizer, "lr_scheduler": {"scheduler": scheduler, "interval": "step"}}
 
-    def train_dataloader(self):
-        return DataLoader(self.train_set, batch_size=self.batch_size, num_workers=self.workers)
-
-    def val_dataloader(self):
-        return DataLoader(self.eval_set, batch_size=self.batch_size, num_workers=self.workers)
-
-    def forward(self, x):
-        return self.model(x)
-
+    def train_dataloader(self): # Lightning seems to not be prepared for "eval only" runs, and expects a training set.
+        print("Preparing training dataloader") 
+        return td.DataLoader(self.train_set, batch_size=self.batch_size, collate_fn=self.collator, num_workers=self.workers) if self.exists("train_set") else None
+    def val_dataloader(self): # Lightning also seems to not like runs without an eval set. Another one for the 'todo' list.
+        print("Preparing eval dataloader")
+        return td.DataLoader(self.eval_set, batch_size=self.batch_size, collate_fn=self.collator, num_workers=self.workers) if self.exists("eval_set") else None
+    def test_dataloader(self):
+        print("Preparing test dataloader")
+        return td.DataLoader(self.test_set, batch_size=self.batch_size, collate_fn=self.collator, num_workers=self.workers) if self.exists("test_set") else None
+    def predict_dataloader(self): # This is new, another thing for the todo list. Seems to be a way to optimize for only specific responses.
+        print("Predict dataloader not implemented yet.")
     # ---------------------
     # RELORA METHODS
     # ---------------------
@@ -109,65 +111,34 @@ class ReloraModule(L.LightningModule):
         self.model = self.model.merge_and_unload() # merge lora
 
     def load_model(self, path):
-        self.model = self.model_class.from_pretrained(path, load_in_8bit=True if self.trainer.precision == "int8" else False, torch_dtype=self.load_precision)
-
-    def init_lora(self):
-        """Loads LoRA layer, and optionally turns training mode on for all other layers."""
-        #self.model = prepare_model_for_kbit_training(self.model)
-        #self.model = get_peft_model(self.model, self.config)
-        if self.train_all_params is True:
-            # Non-layer weights should have training turned on.
-            for name, param in self.model.named_parameters():
-                if any(substring in name for substring in self.base_modules): # We should probably do hard-lookups on this, to avoid slant-matches
-                    param.requires_grad = True
-
-    def reset_optimizer(self, in_place=False):
-        """ The ReLoRA optimizer reset method. """
-        for optimizer in self.trainer.optimizers:
-            for group in optimizer.param_groups:
-                for p in group["params"]:
-                    param_state = optimizer.state[p]
-                    if in_place is False:
-                        param_state["exp_avg"] = torch.zeros_like(p.data)
-                        param_state["exp_avg_sq"] = torch.zeros_like(p.data)
-                    else:
-                        param_state["exp_avg"].zero_()
-                        param_state["exp_avg_sq"].zero_()
+        self.model = (self.model_class if self.model_path is None else
+                      self.model_class.from_pretrained(path, load_in_8bit=True if self.trainer.precision == "int8" else False, torch_dtype=self.load_precision))
 
     def on_train_epoch_end(self):
-        #if self.merge_freq > 0: self.current_epoch
         if self.exists("accuracy"):
             self.accuracy.reset()
-    
-    def lora_checkpoint_reset(self, checkpoint: Dict[str, Any]):
-        self.reset_optimizer()
-        self.merge_lora_weights
-        checkpoint["state_dict"] = self.model.state_dict()  #We want to make sure we're not saving the LoRA state dict
-        self.reset_optimizer(in_place=True) if self.trainer.scaler is not None else self.reset_optimizer()    
-        self.init_lora()
-
-        return checkpoint
 
     # ---------------------
     # Helper Methods
     # ---------------------
-    def on_save_checkpoint(self, checkpoint: Dict[str, Any]):
-        """ Handles checkpoint logic """
-        if self.exists("merge_freq") and self.trainer.current_epoch % self.merge_freq == 0: # This is our flag to see if we're using the ReLoRA training method.
-            checkpoint = self.lora_checkpoint_reset(checkpoint)
-        return checkpoint
-
     def exists(self, attribute):
         """ Checks the truthiness of an attribute, good for unit-testing, stacking custom inheritance classes, and conditionals. """
         return bool(hasattr(self, attribute) and bool(getattr(self, attribute)))
-    
+    # We need the number of steps for our optimizer ASAP, but Lightning prefers to calculate this stuff dynamically, and explicit variables are easier to grok.
     @property
-    def max_steps(self):
-        return self.trainer.max_steps
+    def max_global_steps(self): # Max steps rarely carries the proper total step size, so this calculates it manually while I try to track down the trainer's own reference.
+        """ Calculates the total step count required to complete the entire training session over all epochs. """
+        return math.ceil(self.num_batches / self.num_devices) * self.num_epochs # Progress bar only runs along rank 0, so we only need to round up.
     @property
-    def total_steps(self):
-        return int(self.trainer.max_steps if self.trainer.max_steps != -1 # Borrows max_steps if it exists, or
-            else self.trainer.max_epochs * self.trainer.limit_train_batches if isinstance(self.trainer.limit_train_batches, int) # Use limit_train_batches
-            else self.trainer.max_epochs * (math.ceil(self.train_set.__len__() / self.batch_size) * self.trainer.limit_train_batches) if isinstance(self.trainer.limit_train_batches, float)
-            else self.trainer.max_epochs * math.ceil(self.train_set.__len__() / self.batch_size) if self.trainer.max_epochs != -1
-            else 57600)
+    def num_epochs(self):
+        """ Fetches the total number of epochs that we'll be training for """
+        return self.trainer.max_epochs if self.trainer.max_epochs != -1 else None
+    @property
+    def num_batches(self):
+        """ Fetches the total number of batches/steps that the trainer wants to use for a single training pass. """
+        return self.trainer.num_training_batches if self.trainer.num_training_batches not in [-1, math.inf] else math.ceil(self.train_set.__len__() / self.batch_size)
+    @property
+    def num_devices(self):
+        """ Fetches the total number of devices that the trainer will split the data between. """
+        devices = (self.trainer.num_nodes * self.trainer.num_devices)
+        return devices if devices != -1 else 1
